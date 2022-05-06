1aa6f5520deb1ed23218492e44e71db5eba675d8
hudnellmarcus <hudnell.marcus@gmail.com>
2022-05-06 13:10:02 -0700

    Initial commit API added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://odds.p.rapidapi.com/v4/sports?all=true",
+    "method": "GET",
+    "headers": {
+        "X-RapidAPI-Host": "odds.p.rapidapi.com",
+        "X-RapidAPI-Key": "f0a1b48427mshfec022783697cb0p16a91ajsn3f6bcbd50715"
+    }
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+
+});
+
